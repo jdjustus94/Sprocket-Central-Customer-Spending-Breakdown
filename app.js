6683531aabf5b4d0044fdd7d405852274988fc03
jdjustus94 <112405952+jdjustus94@users.noreply.github.com>
2023-08-17 09:16:05 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ console.log('Is this working?');
 let viz;
 
 //Add Share Link to Tableau Public in here
-const url = "https://public.tableau.com/views/SprocketCentralCustomerSpendingBreakdown/SprocketCentralCustomerSpendingBreakdown?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link";
+const url = "https://public.tableau.com/app/profile/joshua.justus/viz/SprocketCentralCustomerSpendingBreakdown/SprocketCentralCustomerSpendingBreakdown?publish=yes";
 
 const vizContainer = document.getElementById('vizContainer');
 const options = {
